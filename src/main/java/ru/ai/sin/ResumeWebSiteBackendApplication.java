305f76bd40dc4a4cf605647a0752ba68fbe454c4
Mihail Moiseev <moiseev.m.work@mail.ru>
2025-11-18 16:59:23 +0300

    Init commit

---
+++ src/main/java/ru/ai/sin/ResumeWebSiteBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.ai.sin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeWebSiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeWebSiteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ai/sin/ResumeWebSiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ai.sin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeWebSiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
