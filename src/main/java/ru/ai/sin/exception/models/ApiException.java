5b21c4b2ca9c68c1144454f5bff4402c13ec5c2f
Med771 <mm7737333@gmail.com>
2025-11-20 15:16:57 +0300

    Feat: Add Exceptions classes

---
+++ src/main/java/ru/ai/sin/exception/models/ApiException.java
@@ -0,0 +1,15 @@
+package ru.ai.sin.exception.models;
+
+public abstract class ApiException extends RuntimeException {
+    private final String code;
+    private final int status;
+
+    protected ApiException(String message, String code, int status) {
+        super(message);
+        this.code = code;
+        this.status = status;
+    }
+
+    public String getCode() { return code; }
+    public int getStatus() { return status; }
+}
