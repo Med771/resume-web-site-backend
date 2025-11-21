403cc70e2a74140a34d8205c4878b54b1f613b94
Med771 <mm7737333@gmail.com>
2025-11-21 19:28:59 +0300

    Feat: Add Join Entities

---
+++ src/main/java/ru/ai/sin/entity/EducationStudentEnt.java
@@ -0,0 +1,46 @@
+package ru.ai.sin.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import ru.ai.sin.entity.model.TimeStamped;
+
+@Entity
+@Table(name = "educations_students")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EducationStudentEnt {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "education_id")
+    private EducationEnt education;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private StudentEnt student;
+
+    @Column(name = "start_year", nullable = false)
+    @Min(1900)
+    @Max(2100)
+    private int startYear;
+
+    @Column(name = "end_year", nullable = false)
+    @Min(1900)
+    @Max(2100)
+    private int endYear;
+
+    @Embedded
+    private TimeStamped timestamps = new TimeStamped();
+}
