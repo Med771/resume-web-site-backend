2787ee28786334a11753b142eb822e466936b686
Mihail Moiseev <moiseev.m.work@mail.ru>
2025-11-24 00:58:17 +0300

    FEAT: add CRUD endpoints for education

---
+++ src/main/java/ru/ai/sin/controller/EducationCnt.java
@@ -0,0 +1,33 @@
+package ru.ai.sin.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.ai.sin.dto.education.AddEducationReq;
+import ru.ai.sin.dto.education.GetEducationRes;
+import ru.ai.sin.dto.education.MergeEducationReq;
+
+@RestController
+@RequestMapping(path = "/admin/education")
+public class EducationCnt {
+
+    @GetMapping(path = "getById")
+    public ResponseEntity<GetEducationRes> getById(@RequestParam long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @PostMapping(path = "create")
+    public ResponseEntity<GetEducationRes> create(@RequestBody AddEducationReq educationReq) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+
+    @PutMapping(path = "merge")
+    public ResponseEntity<GetEducationRes> merge(@RequestBody MergeEducationReq educationReq) {
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @DeleteMapping(path = "deleteById")
+    public ResponseEntity<GetEducationRes> deleteById(@RequestParam long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+}
---
+++ src/main/java/ru/ai/sin/dto/education/MergeEducationReq.java
@@ -0,0 +1,24 @@
+package ru.ai.sin.dto.education;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import ru.ai.sin.dto.skill.GetSkillRes;
+
+import java.util.List;
+
+public record MergeEducationReq(
+        long id,
+
+        @NotBlank
+        @Size(min = 1, max = 255, message = "Institution must be less than 255 characters")
+        String institution,
+
+        @NotBlank
+        @Size(min = 1, max = 2000, message = "Additional info must be less than 2000 characters")
+        String additionalInfo,
+
+        @NotNull
+        List<GetSkillRes> skills
+) {
+}
