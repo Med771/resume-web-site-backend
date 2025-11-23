bd3794c853189bbf4bc52d6f59dbe6f94f81e01f
Mihail Moiseev <moiseev.m.work@mail.ru>
2025-11-24 01:03:54 +0300

    FEAT: add education mapper

---
+++ src/main/java/ru/ai/sin/mapper/EducationMapper.java
@@ -0,0 +1,31 @@
+package ru.ai.sin.mapper;
+
+import org.mapstruct.*;
+import ru.ai.sin.dto.education.AddEducationReq;
+import ru.ai.sin.dto.education.GetEducationRes;
+import ru.ai.sin.dto.education.MergeEducationReq;
+import ru.ai.sin.entity.EducationEnt;
+
+@Mapper(componentModel = "spring")
+public interface EducationMapper {
+
+    // ---------------- AddEducationReq -> Entity ----------------
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "isActive", ignore = true)
+    @Mapping(target = "timestamps", ignore = true)
+    @Mapping(target = "skills", ignore = true)
+    @Mapping(target = "education", ignore = true)
+    EducationEnt toEntity(AddEducationReq dto);
+
+    // ---------------- Entity -> GetEducationRes ----------------
+    @Mapping(source = "timestamps.createdAt", target = "createdAt")
+    @Mapping(source = "timestamps.updatedAt", target = "updatedAt")
+    GetEducationRes toGetRes(EducationEnt entity);
+
+    // ---------------- MergeSkillReq -> Entity ----------------
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "isActive", ignore = true)
+    @Mapping(target = "timestamps", ignore = true)
+    @Mapping(target = "education", ignore = true)
+    void updateEntityFromDto(MergeEducationReq dto, @MappingTarget EducationEnt entity);
+}
