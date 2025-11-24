2b4690434c4d2a09d92cbbf22dba0299787b7f79
Med771 <mm7737333@gmail.com>
2025-11-24 14:28:39 +0300

    Feat: Add Recruit

---
+++ src/main/java/ru/ai/sin/controller/RecruiterCnt.java
@@ -0,0 +1,33 @@
+package ru.ai.sin.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.ai.sin.dto.recruiter.AddRecruiterReq;
+import ru.ai.sin.dto.recruiter.GetRecruiterRes;
+import ru.ai.sin.dto.recruiter.MergeRecruiterReq;
+
+@RestController
+@RequestMapping(path = "/admin/recruiter")
+public class RecruiterCnt {
+
+    @GetMapping(path = "getById")
+    public ResponseEntity<GetRecruiterRes> getById(@RequestParam long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @PostMapping(path = "create")
+    public ResponseEntity<GetRecruiterRes> create(@RequestBody AddRecruiterReq recruiterReq) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(null);
+    }
+
+    @PutMapping(path = "merge")
+    public ResponseEntity<GetRecruiterRes> merge(@RequestBody MergeRecruiterReq recruiterReq) {
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @DeleteMapping(path = "deleteById")
+    public ResponseEntity<GetRecruiterRes> deleteById(@RequestParam long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+}
