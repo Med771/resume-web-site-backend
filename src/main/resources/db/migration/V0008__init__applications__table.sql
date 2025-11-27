8d99e323b4efc0316a803fa7f4c78d135309454f
Med771 <mm7737333@gmail.com>
2025-11-27 14:28:22 +0300

    REVIEW: update Application logic

--- src/main/resources/db/migration/V0008__init__applications__table.sql
+++ src/main/resources/db/migration/V0008__init__applications__table.sql
@@ -20,7 +20,7 @@ ALTER TABLE applications
     ADD CONSTRAINT uc_applications_history_path UNIQUE (history_path);
 
 ALTER TABLE applications
-    ADD CONSTRAINT FK_APPLICATIONS_ON_RECRUITER FOREIGN KEY (recruiter_id) REFERENCES employees (id);
+    ADD CONSTRAINT FK_APPLICATIONS_ON_RECRUITER FOREIGN KEY (recruiter_id) REFERENCES recruiters (id);
 
 ALTER TABLE applications
     ADD CONSTRAINT FK_APPLICATIONS_ON_STUDENT FOREIGN KEY (student_id) REFERENCES students (id);
