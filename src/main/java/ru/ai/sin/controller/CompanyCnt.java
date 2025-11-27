2ee3f1b3a41896f62cfe2a1f7b910fd90f44e52c
Med771 <mm7737333@gmail.com>
2025-11-27 09:55:35 +0300

    REVIEW: delete not active imports

--- src/main/java/ru/ai/sin/controller/CompanyCnt.java
+++ src/main/java/ru/ai/sin/controller/CompanyCnt.java
@@ -3,44 +3,51 @@ package ru.ai.sin.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import ru.ai.sin.dto.company.AddCompanyReq;
-import ru.ai.sin.dto.company.GetCompanyRes;
-import ru.ai.sin.dto.company.MergeCompanyReq;
+import ru.ai.sin.dto.company.CompanyDTO;
+import ru.ai.sin.dto.company.CompanyNameDTO;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/admin/company")
+@RequestMapping(path = "/company")
 public class CompanyCnt {
 
     @GetMapping(path = "/getById")
-    public ResponseEntity<GetCompanyRes> getById(
+    public ResponseEntity<CompanyDTO> getById(
             @RequestParam long id) {
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
+    @GetMapping(path = "/getByName")
+    public ResponseEntity<CompanyDTO> getByName(
+            @RequestBody CompanyNameDTO companyNameDTO) {
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
     @GetMapping(path = "/getAll")
-    public ResponseEntity<List<GetCompanyRes>> getAll(
+    public ResponseEntity<List<CompanyDTO>> getAll(
             @RequestParam(defaultValue = "0") long page,
             @RequestParam(defaultValue = "10") long size) {
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
     @PostMapping(path = "/create")
-    public ResponseEntity<GetCompanyRes> create(
+    public ResponseEntity<CompanyDTO> create(
             @RequestBody AddCompanyReq companyReq) {
         return ResponseEntity.status(HttpStatus.CREATED).body(null);
     }
 
-    @PutMapping(path = "/merge")
-    public ResponseEntity<GetCompanyRes> merge(
-            @RequestParam(defaultValue = "-1") long id,
-            @RequestBody MergeCompanyReq companyReq) {
+    @PostMapping(path = "/setName")
+    public ResponseEntity<CompanyDTO> setName(
+            @RequestParam long id,
+            @RequestBody CompanyNameDTO companyNameDTO) {
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
 
     @DeleteMapping(path = "/deleteById")
-    public ResponseEntity<GetCompanyRes> deleteById(
+    public ResponseEntity<CompanyDTO> deleteById(
             @RequestParam long id) {
         return ResponseEntity.status(HttpStatus.OK).body(null);
     }
