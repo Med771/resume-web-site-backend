c2311b269ca3a08027962a9c1e7d74767d665214
Med771 <mm7737333@gmail.com>
2025-12-01 15:01:14 +0300

    REVIEW: add Institution repo method

--- src/main/java/ru/ai/sin/repository/InstitutionRepo.java
+++ src/main/java/ru/ai/sin/repository/InstitutionRepo.java
@@ -1,7 +1,11 @@
 package ru.ai.sin.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.ai.sin.entity.InstitutionEnt;
 
 public interface InstitutionRepo extends JpaRepository<InstitutionEnt, Long> {
+
+    Page<InstitutionEnt> findAllByEducationId(Long educationId, Pageable pageable);
 }
