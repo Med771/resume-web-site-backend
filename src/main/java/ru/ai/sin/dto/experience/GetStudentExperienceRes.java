72a4f889d3a46c5cec96ccedf17128cdd0c5d25e
Mihail Moiseev <moiseev.m.work@mail.ru>
2025-12-05 15:07:13 +0300

    FEAT: small fix

--- src/main/java/ru/ai/sin/dto/experience/GetStudentExperienceRes.java
+++ src/main/java/ru/ai/sin/dto/experience/GetStudentExperienceRes.java
@@ -3,8 +3,6 @@ package ru.ai.sin.dto.experience;
 import jakarta.validation.constraints.NotNull;
 import ru.ai.sin.dto.company.CompanyRes;
 
-import java.util.UUID;
-
 public record GetStudentExperienceRes(
         @NotNull
         CompanyRes company,
