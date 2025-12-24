6421c7f7e1accae4e02f88d8f306c9f5d366070a
Mihail Moiseev <moiseev.m.work@mail.ru>
2025-12-24 17:15:15 +0300

    Small fix

--- src/main/java/ru/ai/sin/repository/ApplicationRepo.java
+++
@@ -1,9 +0,0 @@
-package ru.ai.sin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.ai.sin.entity.ApplicationEnt;
-
-@Repository
-public interface ApplicationRepo extends JpaRepository<ApplicationEnt, Long> {
-}
