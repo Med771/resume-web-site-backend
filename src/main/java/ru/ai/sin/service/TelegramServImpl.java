9985b36fde57d6b8a7c4b76c7f71e12674858958
Mihail Moiseev <moiseev.m.work@mail.ru>
2025-12-29 10:26:16 +0300

    FEAT: add request

--- src/main/java/ru/ai/sin/service/TelegramServImpl.java
+++ src/main/java/ru/ai/sin/service/TelegramServImpl.java
@@ -9,6 +9,7 @@ import ru.ai.sin.dto.telegram.SetTelegramUserIdReq;
 import ru.ai.sin.dto.telegram.StudentTelegramDTO;
 import ru.ai.sin.entity.RecruiterEnt;
 import ru.ai.sin.entity.StudentEnt;
+import ru.ai.sin.entity.model.ContactInformation;
 import ru.ai.sin.exception.models.BadRequestException;
 import ru.ai.sin.exception.models.NotFoundException;
 import ru.ai.sin.repository.RecruiterRepo;
@@ -52,6 +53,10 @@ public class TelegramServImpl implements TelegramService {
     @Transactional
     public StudentTelegramDTO setStudentTelegramUserId(UUID studentId, SetTelegramUserIdReq setTelegramUserIdReq) {
         StudentEnt studentEnt = telegramTools.getStudentOrThrow(studentId);
+
+        if (studentEnt.getContactInformation() == null) {
+            studentEnt.setContactInformation(new ContactInformation());
+        }
 
         if (studentEnt.getContactInformation().getTelegramUserId() != null) {
             throw new BadRequestException("Telegram user id already set for student: " + studentId);
