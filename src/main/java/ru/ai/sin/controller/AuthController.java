e2ee13de504a33de16e55fc123695538d1e5d739
Mihail Moiseev <moiseev.m.work@mail.ru>
2026-01-04 14:58:32 +0300

    FIX: Small fix

--- src/main/java/ru/ai/sin/controller/AuthController.java
+++ src/main/java/ru/ai/sin/controller/AuthController.java
@@ -2,16 +2,22 @@ package ru.ai.sin.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import ru.ai.sin.dto.auth.LoginRequest;
 import ru.ai.sin.dto.auth.TokenPair;
+
 import ru.ai.sin.helper.CookieHelper;
+
 import ru.ai.sin.service.impl.AuthService;
 
 @RestController
