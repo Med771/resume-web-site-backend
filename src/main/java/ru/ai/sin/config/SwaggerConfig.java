acd870cc7179109ca7e0c2a81110220065afad3e
Mihail Moiseev <moiseev.m.work@mail.ru>
2026-01-04 14:43:10 +0300

    FIX: Added an api stream instead of manual instructions

--- src/main/java/ru/ai/sin/config/SwaggerConfig.java
+++ src/main/java/ru/ai/sin/config/SwaggerConfig.java
@@ -3,13 +3,14 @@ package ru.ai.sin.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import ru.ai.sin.property.SwaggerProperties;
 
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,13 +25,12 @@ public class SwaggerConfig {
                         .title(swaggerProperties.getTitle())
                         .description(swaggerProperties.getDescription())
                         .version(swaggerProperties.getVersion()))
-                .servers(List.of(
-                        new Server()
-                                .url(swaggerProperties.getServers().get(0).getUrl())
-                                .description(swaggerProperties.getServers().get(0).getDescription()),
-                        new Server()
-                                .url(swaggerProperties.getServers().get(1).getUrl())
-                                .description(swaggerProperties.getServers().get(1).getDescription())
-                ));
+
+                .servers(swaggerProperties.getServers().stream()
+                        .map(serv -> new Server()
+                                .url(serv.getUrl())
+                                .description(serv.getDescription()))
+                        .toList()
+                );
     }
 }
