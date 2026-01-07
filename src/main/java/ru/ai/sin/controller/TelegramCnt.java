5063ecf4f39fee8299aef6c064a417e811f140cc
Mihail Moiseev <moiseev.m.work@mail.ru>
2026-01-07 16:45:54 +0300

    FIX: Clean

--- src/main/java/ru/ai/sin/controller/TelegramCnt.java
+++ src/main/java/ru/ai/sin/controller/TelegramCnt.java
@@ -1,15 +1,21 @@
 package ru.ai.sin.controller;
 
 import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.validation.annotation.Validated;
+
 import org.springframework.web.bind.annotation.*;
+
 import ru.ai.sin.dto.telegram.RecruiterTelegramDTO;
 import ru.ai.sin.dto.telegram.SetTelegramUserIdReq;
 import ru.ai.sin.dto.telegram.StudentTelegramDTO;
+
 import ru.ai.sin.service.impl.TelegramService;
 
 import java.util.UUID;
@@ -23,65 +29,65 @@ public class TelegramCnt {
     private final TelegramService telegramService;
 
     @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping(path = "/student/username/{telegramUsername}")
+    @GetMapping(path = "/student/{telegramUsername}")
     public ResponseEntity<StudentTelegramDTO> getStudentTelegramByUsername(
             @PathVariable String telegramUsername
     ) {
         StudentTelegramDTO studentTelegramDTO = telegramService.getStudentTelegramByUsername(telegramUsername);
 
-        return ResponseEntity.status(HttpStatus.OK).body(studentTelegramDTO);
+        return ResponseEntity.ok(studentTelegramDTO);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @GetMapping(path = "/recruiter/username/{telegramUsername}")
+    @GetMapping(path = "/recruiter/{telegramUsername}")
     public ResponseEntity<RecruiterTelegramDTO> getRecruiterTelegramByUsername(
             @PathVariable String telegramUsername
     ) {
         RecruiterTelegramDTO recruiterTelegramDTO = telegramService.getRecruiterTelegramByUsername(telegramUsername);
 
-        return ResponseEntity.status(HttpStatus.OK).body(recruiterTelegramDTO);
+        return ResponseEntity.ok(recruiterTelegramDTO);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping(path = "/student/{id}/setUserId")
+    @PostMapping(path = "/student/{id}")
     public ResponseEntity<StudentTelegramDTO> setStudentTelegramUserId(
             @PathVariable UUID id,
             @Valid @RequestBody SetTelegramUserIdReq setTelegramUserIdReq
     ) {
         StudentTelegramDTO studentTelegramDTO = telegramService.setStudentTelegramUserId(id, setTelegramUserIdReq);
 
-        return ResponseEntity.status(HttpStatus.OK).body(studentTelegramDTO);
+        return ResponseEntity.ok(studentTelegramDTO);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping(path = "/recruiter/{id}/setUserId")
+    @PostMapping(path = "/recruiter/{id}")
     public ResponseEntity<RecruiterTelegramDTO> setRecruiterTelegramUserId(
             @PathVariable UUID id,
             @Valid @RequestBody SetTelegramUserIdReq setTelegramUserIdReq
     ) {
         RecruiterTelegramDTO recruiterTelegramDTO = telegramService.setRecruiterTelegramUserId(id, setTelegramUserIdReq);
 
-        return ResponseEntity.status(HttpStatus.OK).body(recruiterTelegramDTO);
+        return ResponseEntity.ok(recruiterTelegramDTO);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping(path = "/student/{id}/clearUserId")
+    @PutMapping(path = "/student/{id}")
     public ResponseEntity<StudentTelegramDTO> clearStudentTelegramUserId(
             @PathVariable UUID id
     ) {
         StudentTelegramDTO studentTelegramDTO = telegramService.clearStudentTelegramUserId(id);
 
-        return ResponseEntity.status(HttpStatus.OK).body(studentTelegramDTO);
+        return ResponseEntity.ok(studentTelegramDTO);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PutMapping(path = "/recruiter/{id}/clearUserId")
+    @PutMapping(path = "/recruiter/{id}")
     public ResponseEntity<RecruiterTelegramDTO> clearRecruiterTelegramUserId(
             @PathVariable UUID id
     ) {
         RecruiterTelegramDTO recruiterTelegramDTO = telegramService.clearRecruiterTelegramUserId(id);
 
-        return ResponseEntity.status(HttpStatus.OK).body(recruiterTelegramDTO);
+        return ResponseEntity.ok(recruiterTelegramDTO);
     }
 }
 
